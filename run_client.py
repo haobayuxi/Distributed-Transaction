65d7b72421c63def839891acd0899f1dc585d63b
wuhao2016 <whaohit@gmail.com>
2022-04-10 15:37:10 +0800

    add run client py

---
+++ run_client.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+
+from http import client
+import subprocess
+import sys
+import logging
+from logging import Logger
+
+TMP_DIR = '~/'
+
+logger = logging.getLogger('')
+
+
+def run_clients():
+    client_id = 0
+    while client_id < 100:
+        cmd = ["./tapir_client"]
+        Logger.info("running client %d", client_id)
+        res = subprocess.call(cmd)
+        client_id += 1
+
+
+def main():
+    logger.setLevel(logging.DEBUG)
+    logger.info("start")
+    run_clients()
+
+
+if __name__ == "__main__":
+    main()
