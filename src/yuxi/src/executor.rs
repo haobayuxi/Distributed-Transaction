736112461321e11cfe8d6cdf5d5107fa47cd81a4
wuhao2016 <whaohit@gmail.com>
2022-09-06 20:42:05 +0800

    update

--- src/yuxi/src/executor.rs
+++ src/yuxi/src/executor.rs
@@ -98,7 +98,7 @@ impl Executor {
                         //     self.msg_queue_index, i, COUNT
                         // );
                         if i == COUNT {
-                            sleep(Duration::from_secs(2));
+                            // sleep(Duration::from_secs(2));
                         }
                         sleep(Duration::from_millis(20));
                     }
