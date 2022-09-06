6f491a2fb911d30054e5f1994d51d33713cec4d2
wuhao2016 <whaohit@gmail.com>
2022-09-06 21:32:06 +0800

    update

--- src/yuxi/src/coordinator.rs
+++ src/yuxi/src/coordinator.rs
@@ -44,7 +44,7 @@ impl YuxiCoordinator {
             txn: YuxiMsg::default(),
             servers: HashMap::new(),
             recv,
-            workload: YcsbQuery::new(config.zipf_theta, config.req_per_query as i32, 100),
+            workload: YcsbQuery::new(config.zipf_theta, config.req_per_query as i32, read_perc),
             config,
             txns_per_client,
         }
