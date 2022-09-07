afb8e2555c630fddd6adcd4814dd75a6bb9b4c34
wuhao2016 <whaohit@gmail.com>
2022-09-07 19:57:51 +0800

    update

--- src/yuxi/src/peer.rs
+++ src/yuxi/src/peer.rs
@@ -159,12 +159,13 @@ impl Peer {
                             COUNT
                         );
                         COUNT += 1;
-                        self.executor_senders
-                            .get(&executor_id)
-                            .unwrap()
-                            .send(msg)
-                            .await
-                            .unwrap();
+                        msg.callback.send(Ok(YuxiMsg::default())).await.unwrap();
+                        // self.executor_senders
+                        //     .get(&executor_id)
+                        //     .unwrap()
+                        //     .send(msg)
+                        //     .await
+                        //     .unwrap();
                         // let index = self.msg_queue_index[executor_id as usize];
                         // IN_MEMORY_MQ[executor_id as usize][index] = Some(msg);
                         // self.msg_queue_index[executor_id as usize] += 1;
