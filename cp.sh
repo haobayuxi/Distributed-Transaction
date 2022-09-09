a5106d55978e9854da741aab4729f46715ea84f1
wuhao2016 <whaohit@gmail.com>
2022-09-09 23:09:40 +0800

    update

--- cp.sh
+++ cp.sh
@@ -7,7 +7,7 @@ cp target/release/dast_client ~/yuxi
 cp target/release/dast_server ~/yuxi
 cp target/release/yuxi_client ~/yuxi
 cp target/release/yuxi_server ~/yuxi
-cp run_client.py ~/yuxi
+# cp run_client.py ~/yuxi
 #cp aggregate_result.py ~/yuxi
 cp clean.sh ~/yuxi
 
@@ -19,6 +19,6 @@ cp target/release/dast_client ~/client
 cp target/release/dast_server ~/client
 cp target/release/yuxi_client ~/client
 cp target/release/yuxi_server ~/client
-cp run_client.py ~/client
+# cp run_client.py ~/client
 #cp aggregate_result.py ~/client
 cp clean.sh ~/client
