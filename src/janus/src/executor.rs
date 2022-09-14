f0f272ba4b5142c78a278dc7a587d71dc97a3382
wuhao2016 <whaohit@gmail.com>
2022-09-14 14:09:47 +0800

    update

--- src/janus/src/executor.rs
+++ src/janus/src/executor.rs
@@ -113,36 +113,36 @@ impl Executor {
             node.txn.deps = commit.txn.deps;
             node.committed = true;
 
-            // let mut executed = true;
-            // for dep in node.txn.deps.iter() {
-            //     if *dep == 0 {
-            //         continue;
-            //     }
-            //     let (dep_clientid, dep_index) = get_txnid(*dep);
-            //     while TXNS[dep_clientid as usize].len() <= dep_index as usize
-            //         || !TXNS[dep_clientid as usize][dep_index as usize].committed
-            //     {
-            //         // not committed
-            //         executed = false;
-            //         break;
-            //     }
-            //     if executed {
-            //         let next = &mut TXNS[dep_clientid as usize][dep_index as usize];
-            //         if next.executed {
-            //             continue;
-            //         } else {
-            //             executed = false;
-            //             break;
-            //         }
-            //     } else {
-            //         break;
-            //     }
-            // }
-            // if executed {
-            //     node.executed = true;
-            //     // println!("exec execute {}", txnid);
-            //     self.apply.send(txnid);
-            // }
+            let mut executed = true;
+            for dep in node.txn.deps.iter() {
+                if *dep == 0 {
+                    continue;
+                }
+                let (dep_clientid, dep_index) = get_txnid(*dep);
+                while TXNS[dep_clientid as usize].len() <= dep_index as usize
+                    || !TXNS[dep_clientid as usize][dep_index as usize].committed
+                {
+                    // not committed
+                    executed = false;
+                    // break;
+                }
+                if executed {
+                    let next = &mut TXNS[dep_clientid as usize][dep_index as usize];
+                    if next.executed {
+                        continue;
+                    } else {
+                        executed = false;
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+            if executed {
+                node.executed = true;
+                // println!("exec execute {}", txnid);
+                self.apply.send(txnid);
+            }
         }
     }
 
