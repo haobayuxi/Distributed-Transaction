7f1577f4e907090669433b6cadc14ed416d6bddf
wuhao2016 <whaohit@gmail.com>
2022-09-16 14:06:07 +0800

    update

--- aggregate_result.py
+++ aggregate_result.py
@@ -30,6 +30,7 @@ def per_server_throughput(id):
     file_name = str(id) + "throughput.data"
     f = open(file_name)
     x = f.readlines(15)
+    print(x)
     for i in range(0, 15):
         result.append(float(x[i].strip('\n')))
     f.close()
