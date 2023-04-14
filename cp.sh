ab4844c2b3a2fa36511316970bdd852de8263fb1
haobayuxi <whaohit@gmail.com>
2023-04-14 21:40:23 +0800

    update

--- cp.sh
+++ cp.sh
@@ -1,24 +1,19 @@
 #!/yuxi/bash
-cp target/release/meerkat_client ~/yuxi
-cp target/release/meerkat_server ~/yuxi
-cp target/release/janus_client ~/yuxi
-cp target/release/janus_server ~/yuxi
-cp target/release/dast_client ~/yuxi
-cp target/release/dast_server ~/yuxi
-cp target/release/yuxi_client ~/yuxi
-cp target/release/yuxi_server ~/yuxi
+
+cp target/release/*server ~/server0
+
+
+cp target/release/*server ~/server1
+
+cp target/release/*server ~/server2
+
 # cp run_client.py ~/yuxi
 #cp aggregate_result.py ~/yuxi
 cp clean.sh ~/yuxi
 
-cp target/release/meerkat_client ~/client
-cp target/release/meerkat_server ~/client
-cp target/release/janus_client ~/client
-cp target/release/janus_server ~/client
-cp target/release/dast_client ~/client
-cp target/release/dast_server ~/client
-cp target/release/yuxi_client ~/client
-cp target/release/yuxi_server ~/client
+cp target/release/*client ~/client0
+cp target/release/*client ~/client1
+cp target/release/*client ~/client2
 # cp run_client.py ~/client
 #cp aggregate_result.py ~/client
 cp clean.sh ~/client
