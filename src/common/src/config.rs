be9e9ec38cc9b1df9c0a3db8d3f9ff9f047fbcf9
haobayuxi <whaohit@gmail.com>
2023-04-15 16:46:21 +0800

    update

--- src/common/src/config.rs
+++ src/common/src/config.rs
@@ -46,7 +46,7 @@ impl Default for Config {
         shards.insert(1, vec![0, 1, 2]);
         Self {
             server_addrs,
-            executor_num: 10,
+            executor_num: 20,
             req_per_query: 1,
             table_size: 1000,
             // zipf_theta: 0.0,
